be5b1f6d02b825a5a6114161bb2e022594fa5121
Muhammad Jalloh <dev@TechBuktu.com>
2019-11-23 00:39:23 -0600

    Updated android .../MainActivity.java file with react-navigation settings

--- android/app/src/main/java/com/trendly/MainActivity.java
+++ android/app/src/main/java/com/trendly/MainActivity.java
@@ -3,6 +3,9 @@ package com.trendly;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -12,4 +15,16 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "Trendly";
   }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
+
+
 }
