388b594c49429278f62fe849e7322687057dd233
Muhammad Jalloh <dev@TechBuktu.com>
2019-11-23 23:29:00 -0600

    Added exportable StyleSheet file for Home component

---
+++ mobile/styles/home.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native'
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+
+const homeStyles = StyleSheet.create({ })
+
+
+export default homeStyles
