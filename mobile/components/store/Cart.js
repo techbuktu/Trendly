2ab2c9f756485e93dad0f23e5110dd8127d8f1cb
Muhammad Jalloh <dev@TechBuktu.com>
2019-11-27 16:08:56 -0600

    Barebones shape of Cart component done

--- mobile/components/store/Cart.js
+++ mobile/components/store/Cart.js
@@ -1,16 +1,46 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet  } from 'react-native'
+import { Text, View, ScrollView, StyleSheet  } from 'react-native'
 
 class Cart extends Component {
     constructor(props){
         super(props)
+        this.state = {
+            _id:1, 
+            product_list : [
+                {   'name': 'Product One',
+                    'price': 7,
+                    '_id': 1
+                },
+                {   'name': 'Product Two',
+                    'price': 17,
+                    '_id': 2
+                },
+                {   'name': 'Product Three',
+                    'price': 27,
+                    '_id': 3
+                }
+            ]
+        }
+    }
+
+    getCartDetails(){
+        //CartApi.getCartInfo(cart_id) and then setState({})
     }
     
     render() {
         return (
-            <View>
-                <Text> UI for list of products added to a User's Cart. </Text>
-            </View>
+            <React.Fragment>
+                <View>
+                    <Text> UI for list of products added to a User's Cart. </Text>
+                </View>
+                <ScrollView>
+                    {this.state.product_list.map(product => (
+                        <View>
+                            <Text> {product.name}: ${product.price} </Text>
+                        </View>
+                    ))}
+                </ScrollView>
+            </React.Fragment>
         )
     }
 }
--- mobile/navigators/productsNav.js
+++ mobile/navigators/productsNav.js
@@ -17,7 +17,7 @@ const productsNav = createStackNavigator(
         Order: { screen: Order}
     },
     {
-        initialRouteName: "Categories"
+        initialRouteName: "Cart"
     })
 
 export default productsNav
