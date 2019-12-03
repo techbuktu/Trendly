3ce64e12428855a655c7fda64f9e6c9b473b6bfc
Muhammad Jalloh <dev@TechBuktu.com>
2019-12-03 14:59:54 -0600

    Category model schema created!

--- backend/models/store/Category.js
+++ backend/models/store/Category.js
@@ -3,6 +3,22 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema 
 
 const CategorySchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    addedDate: {
+        type: Date,
+        default: Date.now
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
 
 })
 
