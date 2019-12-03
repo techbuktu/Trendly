677e0cbe5ca824ac296275975d0e6962c4fd8bac
Muhammad Jalloh <dev@TechBuktu.com>
2019-12-03 15:01:56 -0600

    Product model schema creted!

--- backend/models/store/Product.js
+++ backend/models/store/Product.js
@@ -3,7 +3,25 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema 
 
 const ProductSchema = new Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: Map
+    },
+    imageUrl: {
+        type: String,
+        required: false
+    },
+    addedDate: {
+        type: Date,
+        default: Date.now
+    },
+    price: {
+        type: Number,
+        required: true
+    }
 })
 
 module.exports = Product = mongoose.model('Product', ProductSchema)
