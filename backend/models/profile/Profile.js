6d34a1d874cf560ed81aa67bd296cb2f045bfd2c
Muhammad Jalloh <dev@TechBuktu.com>
2019-12-03 14:31:22 -0600

    Profile model created

--- backend/models/profile/Profile.js
+++ backend/models/profile/Profile.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema 
+const slug_generator = require('mongoose-slug-generator')
 
 const ProfileSchema = new Schema({
-
+    user: {
+        Map
+    },
+    about: {
+        type: String,
+        required: true
+    },
 })
 
 module.exports = Profile = mongoose.model('Profile', ProfileSchema)
