cdbc420ee0e2de2de1129dd4d1a651d3b98c1c00
Muhammad Jalloh <dev@TechBuktu.com>
2019-12-03 15:04:59 -0600

    Cart model schema created

--- backend/models/store/Cart.js
+++ backend/models/store/Cart.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema 
 
 const CartSchema = new Schema({
-
+    owner: {
+        type: Map,
+        required: true
+    },
+    product_list: {
+        type: Array
+    }
 })
 
 module.exports = Cart = mongoose.model('Cart', CartSchema)
