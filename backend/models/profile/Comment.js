77969397f951d3325d6e927cb586b96bf10f9c2d
Muhammad Jalloh <dev@TechBuktu.com>
2019-12-03 14:36:56 -0600

    Comment model schema done!

--- backend/models/profile/Comment.js
+++ backend/models/profile/Comment.js
@@ -3,6 +3,21 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema 
 
 const CommentSchema = new Schema({
+    user: {
+        type: Map,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    datePosted: {
+        type: Date,
+        default: Date.now 
+    },
+    targetId: {
+        type: Number
+    }
 
 })
 
