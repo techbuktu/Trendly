c7c198ea3dff9c358fb4d2cc45187f919f87bba8
Muhammad Jalloh <dev@TechBuktu.com>
2019-12-03 22:29:50 -0600

    Feed API routes implemented!

--- backend/api/profile/feeds.js
+++ backend/api/profile/feeds.js
@@ -1,7 +1,8 @@
 const express = require("express")
 const router = express.Router()
 
-const Feed = require('../../models/feeds/Feed')
+const Feed = require('../../models/profile/Feed')
+const Profile = require('../../models/profile/Profile')
 
 /**
  * @route GET api/feeds
@@ -9,17 +10,28 @@ const Feed = require('../../models/feeds/Feed')
  * @access Public 
  */
 router.get('/', (req, res) => {
-    res.send('Success')
+    Profile.find({})
+        .then(feeds => {
+            res.json({
+                successMsg: `${feeds.length} Feeds were found.`,
+                feed_list: feeds
+            })
+        })
+        .catch(err => {
+            res.status(400).json({
+                errorMsg:  `No Feeds have been published.`,
+                error: err
+            })
+        })
 })
-
 
 /**
  * @route GET api/feeds/for/:profileId
- * @desc  Get the matching Feed with _id of :feedId
+ * @desc  Get all matching feeds posted by Profile instance with matching :profileId
  * @access Public 
  */
-router.get('/:feedId', (req, res) => {
-    res.send('Matching Feed with :feedId')
+router.get('/for/:profileId', (req, res) => {
+    
 
 })
 
@@ -29,7 +41,18 @@ router.get('/:feedId', (req, res) => {
  * @access Public 
  */
 router.get('/:feedId', (req, res) => {
-    res.send('Matching Feed with :feedId')
+    Feed.findOne({ _id: req.params.feedId})
+        .then(feed => {
+            res.json({
+                feed: feed
+            })
+        })
+        .catch(err => {
+            res.status(400).json({
+                errorMsg: `Feed not found. Please, check the Feed Id and try again.`,
+                error: err
+            })
+        })
 
 })
 
@@ -39,7 +62,41 @@ router.get('/:feedId', (req, res) => {
  * @access Public 
  */
 router.post('/', (req, res) => {
-    res.send('Create a new Feed instance')
+    const {content, profileId} = req.body
+
+    if(!content || !profileId){
+        return res.status(400).json({
+            errorMsg: `Please, send all required fields' data.`
+        })
+    }
+
+    //Check for the existence of a duplicate Feed
+    Feed.findOne({ profileId: profileId, content: content})
+        .then(duplicateFeed => {
+            if(duplicateFeed){
+                res.status(400).json({
+                    errorMsg: `This Feed already exists.`
+                })
+            } else {
+                //Create the new Feed 
+                const newFeed = new Feed({
+                    profileId: profileId, content: content
+                })
+
+                newFeed.save()
+                    .then(new_feed => {
+                        res.json({
+                            new_feed: new_feed
+                        })
+                    })
+                    .catch(err => {
+                        res.status(400).json({
+                            errorMsg: `New Feed could not be created.`,
+                            error: err
+                        })
+                    })
+            }
+        })
 })
 
 
@@ -49,7 +106,33 @@ router.post('/', (req, res) => {
  * @access Private
  */
 router.put('/:feedId', (req, res) => {
-    res.send('Update matching Feed')
+    // Make sure the Feed exists
+    Feed.findOne({ _id: req.params.feedId})
+        .then(feed => {
+            const updatedFields = Object.keys(req.body)
+
+            //Initialize an object with fields to update
+            let feedUpdates = {}
+            updatedFields.mao(field => {
+                feedUpdates[field] = req.body[field]
+            })
+
+            let query = {_id: req.params.feedId}
+
+            Feed.updateOne(query, feedUpdates)
+                .then(updatedFeed => {
+                    res.json({
+                        updated_feed: updatedFeed
+                    })
+                })
+                .catch(updateError => {
+                    res.status(500).json({
+                        errorMsg: `This Feed (id: ${req.params.feedId}) could not be updated.`,
+                        error: updateError
+                    })
+                })
+        })
+        .catch(err => res.status(400).json({error: err}))
 })
 
 /**
@@ -58,10 +141,18 @@ router.put('/:feedId', (req, res) => {
  * @access Private
  */
 router.delete('/:feedId', (req, res) => {
-    res.send('Delete the matching Feed')
+    Feed.findById(req.params.feedId)
+        .then(feed => {
+            feed.remove()
+                then(() => {
+                    successMsg: `You have deleted this Feed (id: ${req.params.feedId})`
+                })
+        })
+        .catch(err => {
+            res.status(404).json({
+                error: err
+            })
+        })
 })
-
-
-
 
 module.exports = router 
