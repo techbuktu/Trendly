7d0825cb1675f52ef1912211a023e65aa5743082
Muhammad Jalloh <dev@TechBuktu.com>
2019-12-12 18:09:45 -0600

    Changed default backend port to 300 for npw

--- backend/Server.js
+++ backend/Server.js
@@ -53,7 +53,7 @@ db
     console.log(`MongoDB connection error: ${err}`)
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
