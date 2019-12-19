0dd0d64af489a23436c90f487e068957f5af43dc
Muhammad Jalloh <dev@TechBuktu.com>
2019-12-19 17:09:24 -0600

    CategoryApi.getProductsinCategory() added

--- mobile/api/store/CategoryApi.js
+++ mobile/api/store/CategoryApi.js
@@ -4,7 +4,7 @@ class CategoryApi {
     /**
      * Make CRUD API calls against the backend Category REST endpoint at /api/store/categories
      */
-    
+
      static getAllCategories(){
          return Axios.get(`/categories`)
      }
@@ -17,12 +17,16 @@ class CategoryApi {
          return Axios.get(`/categories/${category_id}`)
      }
 
-     static updateCategory(category_id, updated_category_obj){
+    static updateCategory(category_id, updated_category_obj){
          return Axios.put(`/categories/${category_id}`, updated_category_obj)
     }
 
     static deleteCategory(category_id){
         return Axios.delete(`/categories/${category_id}`)
+    }
+
+    static getProductsinCategory(category_id){
+        return Axios.get(`/products/in/${category_id}`)
     }
 
 }
