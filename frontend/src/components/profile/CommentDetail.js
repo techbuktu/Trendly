963b0794f09e4a86e7d8dfdd8076e751e7e7b3dd
Muhammad Jalloh <dev@TechBuktu.com>
2020-03-23 19:01:11 -0500

    Created skeletal structure of 'profile'-based components

---
+++ frontend/src/components/profile/CommentDetail.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class CommentDetail extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default CommentDetail
---
+++ frontend/src/components/profile/LogIn.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class LogIn extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default LogIn
