1e2183d87d4572d970310beabe1120375e22d72d
Muhammad Jalloh <dev@TechBuktu.com>
2020-03-23 19:01:56 -0500

    Home component bootstrapped

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Home extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Home
