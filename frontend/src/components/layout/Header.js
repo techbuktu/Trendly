acf46b437564c9d8eeaad71e0c44a3e368103d4d
Muhammad Jalloh <dev@TechBuktu.com>
2020-03-24 11:57:45 -0500

    Implemented prelim UI and functionality of layout/Header component

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <div>
-            
+            <span>Trendly</span>
+            <button> <Link to="/"> Home </Link> </button>
+            <button> <Link to="/"> Register</Link> </button>
+            <button> <Link to="/"> Shop </Link> </button>
+            <button> <Link to="/"> Sign Out</Link> </button>
         </div>
     )
 }
