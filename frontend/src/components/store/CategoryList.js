e947f935305baadcac424357704289d97d96cd4e
Muhammad Jalloh <dev@TechBuktu.com>
2020-03-26 15:42:38 -0500

    Fixed error with using resulting res.data object in CategoryList

--- frontend/src/components/store/CategoryList.js
+++ frontend/src/components/store/CategoryList.js
@@ -3,25 +3,27 @@ import PropTypes from 'prop-types'
 import CategoryApi from '../../api/store/CategoryApi'
 
 class CategoryList extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            category_list: []
-        }
-
-
-    }
     static propTypes = {
 
     }
 
-    getCategories(){
-        let category_list = []
+    constructor(props){
+        super(props)
+        this.state = {
+            category_list: [1,2,3,4,5]
+        }
+    }
 
+    componentDidMount(){
+        this.getCategories();
+    }
+
+    getCategories(){
         CategoryApi.getAllCategories()
             .then(res => {
+                //console.log(`res.data: ${Object.keys(res.data)}`)
                 this.setState({
-                    category_list
+                    category_list: res.data.category_list
                 }, () => {
                     console.log(`this.state.category_list: ${this.state.category_list}`)
                 })
@@ -30,20 +32,32 @@ class CategoryList extends Component {
     }
 
     render() {
-        const categoryUI = this.state.category_list.map(category => {
-            return (
-                <li key={category.id}>
-                    {category.name}
-                </li>
-            )
-        })
+        
+            if(this.state.category_list.length > 0){
+                console.log('this.state.category_list available.')
+            }
+
+            const categoryUI = this.state.category_list.map(category => {
+                return(
+                    <li>
+                        {category.name}
+                    </li>
+                )
+            })
+            
 
         return (
             <div>
                 List of Product Categories available in this <strong>eStore.</strong>
+                <p>
+                    See the list below.
+                </p>
                 <ul>
                     {categoryUI}
                 </ul>
+                <p>
+                    See the list above.
+                </p>
             </div>
         )
     }
